8075f99a62c0c072cd5e8eaab4aa1684d98ea169
Tomas Doran <tdoran@yelp.com>
2015-03-30 12:38:41 -0700

    Launch a consul cluster

--- eucentral1-demo/internal.tf
+++ eucentral1-demo/internal.tf
@@ -14,16 +14,33 @@ resource "terraform_state" "vpc" {
     }
 }
 
-resource "aws_instance" "internal-other" {
-    ami = "${module.ami.ami_id}"
+resource "aws_launch_configuration" "consul" {
+    image_id = "${module.ami.ami_id}"
     instance_type = "m3.large"
-    tags {
-        Name = "internal-other"
-    }
-    key_name = "${terraform_state.vpc.output.admin_key_name}"
-    subnet_id = "${terraform_state.vpc.output.primary-az-dedicatedsubnet}"
-    private_ip = "${terraform_state.vpc.output.networkprefix}.1.5"
     security_groups = ["${terraform_state.vpc.output.security_group_allow_all}"]
-    user_data = "#cloud-config\napt_sources:\n - source: \"deb https://get.docker.io/ubuntu docker main\"\n   keyid: 36A1D7869245C8950F966E92D8576A8BA88D21E9\n - source: \"deb http://apt.puppetlabs.com trusty main\"\n   keyid: 1054b7a24bd6ec30\napt_upgrade: true\nlocale: en_US.UTF-8\npackages:\n - lxc-docker\n - puppet\n - git\nruncmd:\n - [ /usr/bin/docker, run, -d, --name, consul, -p, \"8500:8500\", -p, \"8600:8600/udp\", fhalim/consul ]\n - [ /usr/bin/docker, run, --rm, -v, \"/usr/local/bin:/target\", jpetazzo/nsenter ]\n"
+    associate_public_ip_address = false
+    user_data = "#cloud-config\napt_sources:\n - source: \"deb https://get.docker.io/ubuntu docker main\"\n   keyid: 36A1D7869245C8950F966E92D8576A8BA88D21E9\n - source: \"deb http://apt.puppetlabs.com trusty main\"\n   keyid: 1054b7a24bd6ec30\napt_upgrade: true\nlocale: en_US.UTF-8\npackages:\n - lxc-docker\n - puppet\n - git\nruncmd:\n - [ /usr/bin/docker, run, -d, --name, consul, -p, \"8500:8500\", -p, \"8600:8600/udp\", bobtfish/consul-awsnycast ]\n - [ /usr/bin/docker, run, --rm, -v, \"/usr/local/bin:/target\", jpetazzo/nsenter ]\n"
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "aws_autoscaling_group" "consul" {
+  availability_zones = ["${terraform_state.vpc.output.primary-az}", "${terraform_state.vpc.output.secondary-az}"]
+  name = "consul"
+  max_size = "${var.size}"
+  min_size = "${var.size}"
+  desired_capacity = "${var.size}"
+  health_check_grace_period = 300
+  health_check_type = "EC2"
+  force_delete = true
+  launch_configuration = "${aws_launch_configuration.consul.name}"
+  vpc_zone_identifier = [ "${terraform_state.vpc.output.primary-az-ephemeralsubnet}", "${terraform_state.vpc.output.secondary-az-ephemeralsubnet}" ]
+  key_name = "${terraform_state.vpc.output.admin_key_name}"
+  tag {
+    key = "Name"
+    value = "consul"
+    propagate_at_launch = true
+  }
 }
 
--- eucentral1-demo/variables.tf
+++ eucentral1-demo/variables.tf
@@ -1,3 +1,6 @@
+variable "size" {
+  default = 3
+}
 variable "region" {}
 variable "account" {}
 variable "github_username" {}
