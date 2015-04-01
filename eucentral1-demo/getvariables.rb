41f1e5cf844e729084d046aac669d64ae8f00840
Tomas Doran <tdoran@yelp.com>
2015-04-01 15:44:54 -0700

    Use a module instead

--- eucentral1-demo/getvariables.rb
+++ eucentral1-demo/getvariables.rb
@@ -2,19 +2,6 @@
 require 'json'
 require 'net/https'
 
-def get(text)
-  uri = URI.parse(text)
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  request = Net::HTTP::Get.new(uri.request_uri)
-
-  response = http.request(request)
-  response.body.gsub('${', '$${')
-end
-
-kubernates_master_ud = get 'https://raw.githubusercontent.com/GoogleCloudPlatform/kubernetes/master/docs/getting-started-guides/aws/cloud-configs/master.yaml'
-kubernates_node_ud = get 'https://raw.githubusercontent.com/GoogleCloudPlatform/kubernetes/master/docs/getting-started-guides/aws/cloud-configs/node.yaml'
-
 output = {
   "variable" => {
     "deploy_ssh_pubkey" => {
@@ -24,12 +11,6 @@ output = {
     "etcd_discovery_uri" => {
       "description" => "An etcd cluster discovery token",
       "default" => IO.read(File.dirname(__FILE__) + '/../etcd_discovery_uri').chomp
-    },
-    "kubernates-master-user-data" => {
-      "default" => kubernates_master_ud
-    },
-    "kubernates-node-user-data" => {
-      "default" => kubernates_node_ud
     },
   }
 }
--- eucentral1-demo/internal.tf
+++ eucentral1-demo/internal.tf
@@ -1,72 +1,17 @@
-module "coreos_amitype" {
-  source = "github.com/bobtfish/terraform-amitype"
-  instance_type = "m3.large"
-}
-
-module "coreos_ami" {
-  source = "github.com/bobtfish/terraform-coreos-ami"
-  region = "${var.region}"
-  channel = "beta"
-  virttype = "${module.coreos_amitype.ami_type_prefer_hvm}"
-}
-
-resource "aws_launch_configuration" "kubernates-node" {
-    image_id = "${module.coreos_ami.ami_id}"
-    instance_type = "m3.large"
-    security_groups = ["${terraform_state.vpc.output.security_group_allow_all}"]
-    associate_public_ip_address = false
-    user_data = "${var.kubernates-node-user-data}"
-    key_name = "${terraform_state.vpc.output.admin_key_name}"
-    lifecycle {
-        create_before_destroy = true
-    }
-}
-
-resource "aws_autoscaling_group" "kubernates-node" {
-  availability_zones = ["${terraform_state.vpc.output.primary-az}", "${terraform_state.vpc.output.secondary-az}"]
-  name = "kubernates-node"
-  max_size = "${var.size}"
-  min_size = "${var.size}"
-  desired_capacity = "${var.size}"
-  health_check_grace_period = 120
-  health_check_type = "EC2"
-  force_delete = true
-  launch_configuration = "${aws_launch_configuration.kubernates-node.name}"
-  vpc_zone_identifier = [ "${terraform_state.vpc.output.primary-az-ephemeralsubnet}", "${terraform_state.vpc.output.secondary-az-ephemeralsubnet}" ]
-  tag {
-    key = "Name"
-    value = "kubernates-node"
-    propagate_at_launch = true
-  }
-}
-
-resource "aws_launch_configuration" "kubernates-master" {
-    image_id = "${module.coreos_ami.ami_id}"
-    instance_type = "m3.large"
-    security_groups = ["${terraform_state.vpc.output.security_group_allow_all}"]
-    associate_public_ip_address = false
-    user_data = "${var.kubernates-master-user-data}"
-    key_name = "${terraform_state.vpc.output.admin_key_name}"
-    lifecycle {
-        create_before_destroy = true
-    }
-}
-
-resource "aws_autoscaling_group" "kubernates-master" {
-  availability_zones = ["${terraform_state.vpc.output.primary-az}", "${terraform_state.vpc.output.secondary-az}"]
-  name = "kubernates-master"
-  max_size = "${var.size}"
-  min_size = "${var.size}"
-  desired_capacity = "${var.size}"
-  health_check_grace_period = 120
-  health_check_type = "EC2"
-  force_delete = true
-  launch_configuration = "${aws_launch_configuration.kubernates-master.name}"
-  vpc_zone_identifier = [ "${terraform_state.vpc.output.primary-az-ephemeralsubnet}", "${terraform_state.vpc.output.secondary-az-ephemeralsubnet}" ]
-  tag {
-    key = "Name"
-    value = "kubernates-master"
-    propagate_at_launch = true
-  }
+module "kubernates" {
+    etcd_discovery_uri = "${var.etcd_discovery_uri}"
+    source = "github.com/bobtfish/terraform-aws-coreos-kubernates-cluster"
+    master-instance_type = "m3.large"
+    node-instance_type = "m3.large"
+    region = "${var.region}"
+    coreos-channel = "beta"
+    sg = "${terraform_state.vpc.output.security_group_allow_all}"
+    admin_key_name = "${terraform_state.vpc.output.admin_key_name}"
+    primary-az = "${terraform_state.vpc.output.primary-az}"
+    secondary-az = "${terraform_state.vpc.output.secondary-az}"
+    master-cluster-size = 3
+    node-cluster-size = 3
+    primary-az-subnet = "${terraform_state.vpc.output.primary-az-ephemeralsubnet}"
+    secondary-az-subnet = "${terraform_state.vpc.output.secondary-az-ephemeralsubnet}"
 }
 
