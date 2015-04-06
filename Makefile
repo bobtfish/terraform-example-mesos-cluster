5ada5873d387173bccd3b2dfb6dfcb1faebe2cf1
Tomas Doran <tdoran@yelp.com>
2015-04-06 16:44:03 -0700

    Grab the admin IP range as a variable

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONEY: all
 
-all: ssh_key
+all: ssh_key admin_iprange.txt
 	true
 
 ssh_key: id_rsa id_rsa.pub
@@ -10,6 +10,9 @@ id_rsa:
 
 id_rsa.pub:
 	ssh-keygen -y -f id_rsa > id_rsa.pub
+
+admin_iprange.txt:
+	echo $$(curl 'http://api.ipify.org?format=text' 2>/dev/null)/32 > admin_iprange.txt
 
 clean:
 	rm id_rsa id_rsa.pub
--- eucentral1-demo/getvariables.rb
+++ eucentral1-demo/getvariables.rb
@@ -8,6 +8,10 @@ output = {
       "description" => "The Deployment ssh pub key",
       "default" => IO.read(File.dirname(__FILE__) + '/../id_rsa.pub').chomp
     },
+    "admin_iprange" => {
+      "description" => "The IP range to lock administration down to",
+      "default" => IO.read(File.dirname(__FILE__) + '/../admin_iprange.txt').chomp
+    }
   }
 }
 
