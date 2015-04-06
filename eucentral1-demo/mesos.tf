c71cfba8a929a83ff85b06c92247f86f6664a631
Tomas Doran <tdoran@yelp.com>
2015-04-06 09:23:05 -0700

    Be able to spell

--- eucentral1-demo/mesos.tf
+++ eucentral1-demo/mesos.tf
@@ -5,7 +5,7 @@ module "mesos" {
     region = "${var.region}"
     admin_key_name = "${terraform_remote_state.vpc.output.admin_key_name}"
 #    az = "${terraform_remote_state.vpc.output.primary-az}"
-    subnet_id = "${terraform_remote_state.vpc.output.primary-az-ehemeralsubnet}"
+    subnet_id = "${terraform_remote_state.vpc.output.primary-az-ephemeralsubnet}"
     domain = "mesos.notanisp.net"
     vpc_id = "${terraform_remote_state.vpc.output.id}"
     ssh_private_key_file = "../id_rsa"
